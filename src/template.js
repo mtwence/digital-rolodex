d2690f59e727fa22249085fc3463b8c7db9be149
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-22 00:06:18 -0400

    add margins to cards

--- src/template.js
+++ src/template.js
@@ -3,7 +3,7 @@ const internal = require("stream");
 const genTeam = (template) => {
   const genManager = (manager) => {
     return `
-            <div class="card employee-card">
+            <div class="card employee-card m-1">
             <div class="card-header">
                 <h2 class="card-title">${manager.getName()}</h2>
                 <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
@@ -20,7 +20,7 @@ const genTeam = (template) => {
   };
   const genEngineer = (engineer) => {
     return `
-            <div class="card employee-card">
+            <div class="card employee-card m-1">
             <div class="card-header">
                 <h2 class="card-title">${engineer.getName()}</h2>
                 <h3 class="card-title"><i class="fas fa-wrench mr-2"></i>${engineer.getRole()}</h3>
@@ -37,7 +37,7 @@ const genTeam = (template) => {
   };
   const genIntern = (intern) => {
     return `
-            <div class="card employee-card">
+            <div class="card employee-card m-1">
             <div class="card-header">
                 <h2 class="card-title">${intern.getName()}</h2>
                 <h3 class="card-title"><i class="fas fa-graduation-cap mr-2"></i>${intern.getRole()}</h3>
