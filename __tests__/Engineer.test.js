f56b491412a96f6397d76fd3487a9955763c38e9
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-21 22:54:07 -0400

    add test for employee constructor

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,2 +1,12 @@
 const Employee = require("../lib/Employee");
 
+describe("Employee", () => {
+    const employee = new Employee("Jeffrey", "007", "Jeff007@gmail.com");
+    console.log(employee);
+    describe("constructor", () => {
+      it("should create name, id, and email", () => {
+        expect(employee.empName).toEqual("Jeffrey");
+        expect(employee.empId).toEqual("007");
+        expect(employee.empEmail).toEqual("Jeff007@gmail.com");
+      });
+    });  
